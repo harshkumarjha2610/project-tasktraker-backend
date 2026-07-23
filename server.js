1ad5a0951b890a6607604b882a2e126fc8f0704a
appdeveloperharshsmsolutionsin-a11y <appdeveloperharsh.smsolutions.in@gmail.com>
2026-07-23 13:48:06 +0530

    rst commit

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
